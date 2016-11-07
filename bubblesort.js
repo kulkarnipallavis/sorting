15ea2b86fa5f979d95547f7988be77e36d0d4999
kulkarnipallavis <kulkarnipallavis@gmail.com>
2016-11-06 19:31:53 -0500

    completed

--- bubblesort.js
+++ bubblesort.js
@@ -20,3 +20,28 @@ var swap = function (arr, index){
 	arr[index+1] = arr[index];
 	arr[index] = temp;
 }
+
+for (var i = 9; i <= 12; i++) {
+  var numItems = Math.pow(2, i);
+  var nativeTestArray = [];
+
+  for(var j = 0; j < numItems; j++) {
+    var rand = Math.floor(Math.random() * numItems);
+    nativeTestArray.push(rand);
+  }
+
+  var bTestArray = nativeTestArray.slice(0);
+  var mTestArray = nativeTestArray.slice(0);
+
+  console.time(numItems + ' native');
+  nativeTestArray.sort(function(a, b){ return a - b; });
+  console.timeEnd(numItems + ' native');
+
+  console.time(numItems + ' bubble');
+  bubbleSort(bTestArray);
+  console.timeEnd(numItems + ' bubble');
+
+  // console.time(numItems + ' merge');
+  // mergeSort(mTestArray);
+  // console.timeEnd(numItems + ' merge');
+}
